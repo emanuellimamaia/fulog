b833c05dac0db54a73f04f11282adafead7cdf11
emanuel lima maia <emanuellimamaia@gmail.com>
2024-07-29 11:35:11 -0400

    feat:prisma repository

--- src/shared/app.module.ts
+++ src/shared/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/infra/prisma/database.module';
 import { AccountModule } from 'src/modules/account.module';
 
 import { CompaniesModule } from 'src/modules/companies.module';
 
 @Module({
-  imports: [CompaniesModule, AccountModule],
+  imports: [CompaniesModule, AccountModule, PrismaService],
   controllers: [],
   providers: [],
 })
