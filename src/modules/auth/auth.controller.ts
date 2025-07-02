d83f5abca7189052ef9678e2b69ac8449c933f6a
EmanuelMaiaAspen <emanuel.maia@aspenbrasil.com.br>
2025-07-02 09:50:08 -0400

    fix: controller login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
 import { AuthUserDto } from './auth-user.dto';
 import { AuthGuard } from '@nestjs/passport';
-@ApiTags('login')
+@ApiTags('Login')
 @Controller('auth')
 export class AuthController {
 
