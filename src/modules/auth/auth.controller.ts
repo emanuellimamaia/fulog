3b755a7bb6ec0ee61e78e81ced0e0e4c1443e489
EmanuelMaiaAspen <emanuel.maia@aspenbrasil.com.br>
2025-07-02 11:22:25 -0400

    fix: controllker

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -11,7 +11,7 @@ export class AuthController {
 
   @ApiExtraModels(AuthUserDto)
   @UseGuards(AuthGuard('local'))
-  @Post('login-user')
+  @Post('login')
   async handle(@Request() req, @Body() data: AuthUserDto) {
     try {
       const { id, role, username } = req.user;
